ad40712cfe7dc9415db4f2e45b19d533f3c2fe1a
Qianli Wu <qianliwu@g.ucla.edu>
2022-12-23 20:25:02 +0800

    Updates

---
+++ precache-manifest.bb030e953fac42481cb2283f34bd0510.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0ec7bb6cf5aa6c8b975381ba754e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66bce8cdcc873c4ee172",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "bce0c001207de696b36f",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "38a7d665ac0f8e254e57",
+    "url": "/static/css/main.6b13a1fd.chunk.css"
+  },
+  {
+    "revision": "078f895ce8bde5aa6ac4",
+    "url": "/static/js/2.81e28692.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.81e28692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bce8cdcc873c4ee172",
+    "url": "/static/js/3.39e4d19b.chunk.js"
+  },
+  {
+    "revision": "bce0c001207de696b36f",
+    "url": "/static/js/4.ec2ecc6b.chunk.js"
+  },
+  {
+    "revision": "38a7d665ac0f8e254e57",
+    "url": "/static/js/main.31c8fd81.chunk.js"
+  },
+  {
+    "revision": "69b4b66f11ecc62c6cef",
+    "url": "/static/js/runtime-main.2acaa3af.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "53fbab677aebd5f042dd90a611bd20b1",
+    "url": "/static/media/baiduLogo.53fbab67.jpg"
+  },
+  {
+    "revision": "83a59da078003bafcbd980f3cd0b8194",
+    "url": "/static/media/casLogo.83a59da0.jfif"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "ef3381a15252f313cd951825f8e8905b",
+    "url": "/static/media/ucla-logo-circle.ef3381a1.png"
+  },
+  {
+    "revision": "be46e8cfb356af9a60efac907e86f8d6",
+    "url": "/static/media/uclaLogo.be46e8cf.png"
+  }
+]);

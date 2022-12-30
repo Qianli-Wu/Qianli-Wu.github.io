d53f62b75bf7edf018b1b79685b14f0ead196601
Qianli Wu <qianliwu@g.ucla.edu>
2022-12-30 23:42:59 +0800

    Added Big Projects Section Added icons for BruinTrade and CryptoSwap

--- src/portfolio.js
+++ src/portfolio.js
@@ -200,33 +200,34 @@ const openSource = {
 
 const bigProjects = {
   title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  subtitle: "SOME PROJECTS THAT I HELPED TO CREATE THEIR TECH",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/bruintradeLogo.svg"),
+      projectName: "BruinTrade",
+      projectDesc: "A trading platform for UCLA community",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://saayahealth.com/"
+          url: "https://bruintrade.com"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/white_cryptoswapLogo.svg"),
+      projectName: "CryptoSwap",
+      projectDesc: "A scalable decentralized exchange",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://cryptoswap.org/"
         }
         //  you can add extra buttons here.
       ]
     },
-    {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      footerLink: [
-        {
-          name: "Visit Website",
-          url: "http://nextu.se/"
-        }
-      ]
-    }
+    
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
